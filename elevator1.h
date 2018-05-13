301a408813d41aa0aca1d8dad7184c2842a8858f
lzp <36122878+LZP999@users.noreply.github.com>
2018-05-13 12:55:04 +0800

    Add files via upload

---
+++ elevator1.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class elevator1
+{
+public:
+	elevator1();
+	~elevator1();
+	int Run(int from, int to);//显示乘客在的位置和目的地；
+};
+
